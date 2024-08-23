24d6cdfe502774ceca957a29f041a9e11f8cd722
Muhamad Fery Hasyim <hasyim.fery27@gmail.com>
2024-08-23 15:26:24 +0700

    feat: create register page

--- js/toggle-password.js
+++ js/toggle-password.js
@@ -1,5 +1,7 @@
 const passwordField = document.getElementById("password");
 const togglePassword = document.querySelector(".password-toggle-icon i");
+const confirmPasswordField = document.getElementById("confirm-password");
+const toggleConfirmPassword = document.querySelector(".password-toggle-icon.password-register-2 i");
 
 togglePassword.addEventListener("click", function () {
   if (passwordField.type === "password") {
@@ -10,5 +12,17 @@ togglePassword.addEventListener("click", function () {
     passwordField.type = "password";
     togglePassword.classList.remove("fa-eye");
     togglePassword.classList.add("fa-eye-slash");
+  }
+});
+
+toggleConfirmPassword.addEventListener("click", function () {
+  if (confirmPasswordField.type === "password") {
+    confirmPasswordField.type = "text";
+    toggleConfirmPassword.classList.remove("fa-eye-slash");
+    toggleConfirmPassword.classList.add("fa-eye");
+  } else {
+    confirmPasswordField.type = "password";
+    toggleConfirmPassword.classList.remove("fa-eye");
+    toggleConfirmPassword.classList.add("fa-eye-slash");
   }
 });
